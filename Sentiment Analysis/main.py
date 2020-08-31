5e4505c08899cd48ff6f8e6bc2fc5de0fde441e7
Vijayakumar Pattanashetti <35587732+vijayakumarpattanashetti@users.noreply.github.com>
2020-08-31 16:11:16 +0530

    Initial Commit

---
+++ Sentiment Analysis/main.py
@@ -0,0 +1,24 @@
+import pickle
+from flask import Flask, request, jsonify
+from model.ml_model import predict_score
+
+#creating a flask app and naming it "text_scorer"
+app = Flask('text_scorer')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    txt = request.get_json()
+    with open(r'.\model\model.bin', 'rb') as f_in:
+        model = pickle.load(f_in)
+        f_in.close()
+    result = predict_score(txt, model)
+
+    result = {
+        'score': str(result)
+    }
+    return jsonify(result)
+
+@app.route('/call', methods=['GET'])
+def call():
+    return "Calling Model..."
+
