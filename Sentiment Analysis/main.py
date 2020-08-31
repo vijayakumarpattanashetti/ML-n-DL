fa385bc3fa2b930f3f5de4aeb370252745e3def2
Vijayakumar Pattanashetti <35587732+vijayakumarpattanashetti@users.noreply.github.com>
2020-08-31 21:58:10 +0530

    Update main.py

--- Sentiment Analysis/main.py
+++ Sentiment Analysis/main.py
@@ -8,7 +8,7 @@ app = Flask('text_scorer')
 @app.route('/predict', methods=['POST'])
 def predict():
     txt = request.get_json()
-    with open(r'.\model\model.bin', 'rb') as f_in:
+    with open('./model/model.bin', 'rb') as f_in:
         model = pickle.load(f_in)
         f_in.close()
     result = predict_score(txt, model)
